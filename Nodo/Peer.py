68958e5209971a532d18bd3ef300bf9625a0a2aa
Cuau1m1 <cuau134679@gmail.com>
2026-01-06 15:40:19 -0600

    ---

--- Nodo/Peer.py
+++ Nodo/Peer.py
@@ -7,7 +7,7 @@ import socket
 
 from servidor import iniciar_servidor
 from cliente import registrar_nodo, gestionar_descarga, publicar_torrent, obtener_lista_torrents, descargar_torrent_tracker
-from utilerias import cargar_estado_descarga, crear_estado_descarga, obtener_ruta_estado
+from utilerias import cargar_estado_descarga, crear_estado_descarga, crear_estado_seeder
 
 def obtener_ip_local_salida():
     try:
